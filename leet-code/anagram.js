a8b0d215bd90e43962e45cefbcd1fe28bd6a699c
Srilekha <srilekhagovvala441@gmail.com>
2025-11-24 22:43:15 +0530

    anagram

---
+++ leet-code/anagram.js
@@ -0,0 +1,8 @@
+var isAnagram = function(s, t) {
+    if (s.length !==t.length) return false;
+    let arr1=s.split("").sort().join("");//join()by default uses comma, if we put join("") then give clean string
+    let arr2=t.split("").sort().join("");
+    return arr1===arr2
+};
+console.log(isAnagram(tata,atta));
+console.log(isAnagram(akka,bakka))
